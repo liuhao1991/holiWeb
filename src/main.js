f4bae582e3969560c2a6d501553d2ed2fc717220
lh <lh@metgs.com>
2019-01-08 15:39:59 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,12 +9,10 @@ Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 axios.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-// axios.defaults.baseURL = 'http://192.168.107.247/shangyu/index.php'
 axios.defaults.baseURL = 'http://localhost/holiday/index.php/'
 
 Vue.prototype.$axios = axios
 Vue.prototype.$qs = qs
-Vue.prototype.readerUrl = 'http://10.139.242.102/readerApi/index.php'
 var vm = new Vue({
   router,
   render: h => h(App)
